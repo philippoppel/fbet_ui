b24e76451e55a6e95f4c633f4d96ecb57545645e
philippoppel <e1427026@student.tuwien.ac.at>
2025-05-07 19:48:53 +0200

    feat: enhance layout metadata and update favicon links for better app integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,9 +37,28 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' sizes='any' />
-        <link rel='apple-icon' href='/apple-icon.png' />
+        <meta name='apple-mobile-web-app-title' content='Fbet' />
+        <meta name='application-name' content='Fbet' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link rel='manifest' href='/manifest.json' />
       </head>
       <body className='font-sans antialiased flex flex-col min-h-screen'>
         <ThemeProvider
